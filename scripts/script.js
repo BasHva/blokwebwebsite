938b5876946ec47316e16133bd2c559bc3677d30
BasHva <bas.witmond@hva.nl>
2024-11-28 13:06:15 +0100

    28/11

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-function openSearch() {
-    document.getElementById('search-overlay').classList.remove('hidden');
-}
-
-function closeSearch() {
-    document.getElementById('search-overlay').classList.add('hidden');
-}
